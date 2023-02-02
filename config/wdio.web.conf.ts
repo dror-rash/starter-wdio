fc5fe4739f3e5a16ef5503dc53446ad40a0c91bb
dror-rash <dror@transmitsecurity.com>
2023-02-02 10:23:17 +0200

    add chrome fake media

--- config/wdio.web.conf.ts
+++ config/wdio.web.conf.ts
@@ -57,7 +57,13 @@ export const config: WebdriverIO.Config = {
       browserName: "chrome",
       acceptInsecureCerts: true,
       "goog:chromeOptions": {
-        args: ["--headless", "--disable-gpu", "--disable-dev-shm-usage"],
+        args: [
+          "--use-fake-ui-for-media-stream",
+          "--use-fake-device-for-media-stream",
+          "--headless",
+          "--disable-gpu",
+          "--disable-dev-shm-usage",
+        ],
       },
       // If outputDir is provided WebdriverIO can capture driver session logs
       // it is possible to configure which logTypes to include/exclude.
